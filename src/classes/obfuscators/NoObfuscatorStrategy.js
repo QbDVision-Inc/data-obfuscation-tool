87790fbafb5655f4ec33447756555ab37ca50552
Ryan Shillington <ryan@qbdvision.com>
2023-12-11 09:18:27 -0500

    Fixing the NoObfuscatorStrategy

--- src/classes/obfuscators/NoObfuscatorStrategy.js
+++ src/classes/obfuscators/NoObfuscatorStrategy.js
@@ -5,19 +5,7 @@ import logger from '../../config/LogConfig.js';
  */
 export class NoObfuscatorStrategy {
 
-  obfuscateUncachedString(string) {
-    const stringPattern = /(\w+)/g; // Matches strings within quotes
-    const obfuscatedString = string?.replace(stringPattern, (match, p1) => `${this.obfuscateUncachedStringHelper(p1)}`);
-
-    logger.debug(`Obfuscate valued ${obfuscatedString}`);
-    return obfuscatedString;
-  }
-
-  obfuscateUncachedStringHelper(value) {
-    if (value?.length > 1) {
-      const [a, b] = value;
-      return `${a}${b}${'X'.repeat(value.length - 2)}`;
-    }
-    return value;
+  obfuscateString(someString) {
+    return someString;
   }
 }
