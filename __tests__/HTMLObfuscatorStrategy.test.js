bfdbb80dac1eea1e64c38cfa520a99379b50b9a3
Doanh Pham <doanhpham@qbdvision.com>
2025-09-26 23:04:25 +0700

    Starting with unit test

---
+++ __tests__/HTMLObfuscatorStrategy.test.js
@@ -0,0 +1,10 @@
+import { HTMLObfuscatorStrategy } from "../src/classes/obfuscators/HTMLObfuscatorStrategy.js";
+
+describe("HTMLObfuscatorStrategy", () => {
+  test("returns the same string for now (placeholder behavior)", () => {
+    const strategy = new HTMLObfuscatorStrategy();
+    const input = "<div class=\"content\">Hello World</div>";
+    const result = strategy.obfuscateString(input);
+    expect(result).toBe(input);
+  });
+});
