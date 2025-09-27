ae167d88a902ed553b561270e1f3712cc71b7f2f
Doanh Pham <doanhpham@qbdvision.com>
2025-09-27 12:35:51 +0700

    Self review

--- src/classes/obfuscators/HTMLObfuscatorStrategy.js
+++ src/classes/obfuscators/HTMLObfuscatorStrategy.js
@@ -9,7 +9,7 @@ import { DictionaryObfuscatorStrategy } from "./DictionaryObfuscatorStrategy.js"
 export class HTMLObfuscatorStrategy extends BaseObfuscatorStrategy {
   constructor() {
     super();
-    this.stringObfuscator = new DictionaryObfuscatorStrategy();
+    this._dictionaryObfuscatorStrategy = new DictionaryObfuscatorStrategy();
   }
 
   obfuscateString(someString) {
@@ -20,32 +20,74 @@ export class HTMLObfuscatorStrategy extends BaseObfuscatorStrategy {
     try {
       const root = parse(someString);
 
-      this.processTextNodes(root);
+      this._processTextNodes(root);
       console.log(root.toString());
 
       return root.toString();
     } catch (error) {
-      return this.stringObfuscator.obfuscateString(someString);
+      return this._dictionaryObfuscatorStrategy.obfuscateString(someString);
     }
   }
 
-  processTextNodes(node) {
+  _processTextNodes(node) {
     // nodeType 3 is a text node. We want to obfuscate text nodes only.
     if (node.nodeType === 3) {
       const text = node.text;
       if (text && text.trim()) {
-        if (!this.isInsideQbdOutput(node)) {
-          node.textContent = this.stringObfuscator.obfuscateString(text);
+        if (!this._isInsideQbdOutput(node)) {
+          node.textContent = this._dictionaryObfuscatorStrategy.obfuscateString(text);
         }
       }
-    } else if (node.childNodes && node.childNodes.length > 0) {
-      node.childNodes.forEach((childNode) => {
-        this.processTextNodes(childNode);
-      });
+    } else {
+      // For element nodes (nodeType === 1), obfuscate attribute values
+      if (node.nodeType === 1 && node.attributes) {
+        this._obfuscateAttributes(node);
+      }
+
+      if (node.childNodes && node.childNodes.length > 0) {
+        node.childNodes.forEach((childNode) => {
+          this._processTextNodes(childNode);
+        });
+      }
     }
   }
 
-  isInsideQbdOutput(node) {
+  _obfuscateAttributes(node) {
+    const protectedAttributes = new Set([
+      'class',
+      'kind',
+      'style',
+      'contenteditable',
+      'referenceblockname',
+      'referenceblock',
+      'process',
+      'unitoperation',
+      'data-record-id',
+      'data-record-model-name',
+      'data-record-column-name',
+      'data-never-approved',
+      'data-record-sub-model-name',
+      'data-record-sub-model-data'
+    ]);
+
+    // Get all attribute names from the node
+    const attributeNames = Object.keys(node.attributes);
+    for (const attName of attributeNames) {
+      const currentValue = node.getAttribute(attrName);
+      if (!currentValue || typeof currentValue !== 'string' || !currentValue.trim()) {
+        continue;
+      }
+
+      if (protectedAttributes.has(attName)) {
+        continue;
+      }
+
+      const obfuscatedValue = this._dictionaryObfuscatorStrategy.obfuscateString(currentValue);
+      node.setAttribute(attrName, obfuscatedValue);
+    }
+  }
+
+  _isInsideQbdOutput(node) {
     let current = node.parentNode;
 
     while (current) {
